2a4304c272fd8d55b0ab6a45459a812b2904bd17
vasswann <swannxxx@yahoo.co.uk>
2026-03-17 11:34:40 +0000

    Add Vehicle base class, Car and Motorcycle subclasses, and Engine abstraction with FuelType enum

---
+++ Vehicles/Engine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    internal abstract class Engine
+    {
+        public bool Running { get; set; }
+        public int Horsepower { get; set; }
+        public FuelType FuelType { get; set; }
+
+        public Engine(int horsepower, FuelType fuelType)
+        {
+            this.Running = false;
+            this.Horsepower = horsepower;
+            this.FuelType = fuelType;
+        }
+    }
+}
---
+++ Vehicles/Motorcycle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    internal class Motorcycle : Vehicle
+    {
+        public bool HasSidecar { get; set; }
+        public Motorcycle(string make, string model, int speed, bool hasSideCar) : base(make, model, speed)
+        {
+            this.HasSidecar = hasSideCar;
+        }
+    }
+}
