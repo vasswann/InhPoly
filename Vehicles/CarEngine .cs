c85b68848863a7544baaee9e052be7b1f4cbdebf
vasswann <swannxxx@yahoo.co.uk>
2026-03-17 11:49:10 +0000

    Implement Engine subclasses with fixed configurations

---
+++ Vehicles/CarEngine .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    internal class CarEngine : Engine
+    {
+        public CarEngine() : base(150, FuelType.Diesel)
+        {
+        }
+    }
+}
---
+++ Vehicles/MotorcycleEngine .cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    internal class MotorcycleEngine : Engine
+    {
+        public MotorcycleEngine() : base(75, FuelType.Unleaded) { }
+    }
+}
